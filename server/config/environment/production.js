19fa9e310c89445a0f6865abe62c5d0f863ebb92
Williams Aguilera <webnator@gmail.com>
2016-07-13 19:37:12 +0200

    MOD: Config variables for mysql

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -6,10 +6,18 @@ module.exports = {
   ip              : 'localhost',
   port            : process.env.PORT || 9000,
 
+  // mysqlSettings: {
+  //   host     : '107.180.21.19',
+  //   port     : '3306',
+  //   user     : 'toptalAdmin',
+  //   password : 'QbX!4{82KOx*',
+  //   database : 'zing_reports'
+  // },
+
   mysqlSettings: {
-    host     : '107.180.21.19',
+    host     : 'ec2-54-201-213-141.us-west-2.compute.amazonaws.com',
     port     : '3306',
-    user     : 'toptalAdmin',
+    user     : 'toptalAPI',
     password : 'QbX!4{82KOx*',
     database : 'zing_reports'
   },
